29374082a48669a114004ef0a8e482dbee4e1bc0
= <petertrotman@gmail.com>
2017-01-27 13:20:15 +0000

    Separate dev and production webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -2,48 +2,13 @@ const { resolve } = require('path');
 const webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
 
-module.exports = {
-  entry: [
-    'react-hot-loader/patch',
-    // activate HMR for React
-
-    'webpack-dev-server/client?http://localhost:8080',
-    // bundle the client for webpack-dev-server
-    // and connect to the provided endpoint
-
-    'webpack/hot/only-dev-server',
-    // bundle the client for hot reloading
-    // only- means to only hot reload for successful updates
-
-
-    './index.js'
-    // the entry point of our app
-  ],
+const baseConfig = {
+  context: resolve(__dirname, 'src'),
+  entry: ['./index.js'],
   output: {
     filename: 'bundle.js',
-    // the output bundle
-
     path: resolve(__dirname, 'dist'),
-
     publicPath: '/print-a-gif/dist/'
-    // necessary for HMR to know where to load the hot update chunks
-  },
-
-  context: resolve(__dirname, 'src'),
-
-  devtool: 'inline-source-map',
-
-  devServer: {
-    hot: true,
-    // enable HMR on the server
-
-    contentBase: resolve(__dirname),
-    // match the output path
-
-    publicPath: '/print-a-gif/dist/',
-    // match the output `publicPath`
-
-    historyApiFallback: true,
   },
 
   module: {
@@ -72,11 +37,32 @@ module.exports = {
     ],
   },
 
+  plugins: [],
+};
+
+const devConfig = Object.assign({}, baseConfig, {
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+  ].concat(baseConfig.entry),
+  devtool: 'inline-source-map',
+  devServer: {
+    hot: true,
+    contentBase: resolve(__dirname),
+    publicPath: '/print-a-gif/dist/',
+    historyApiFallback: true,
+  },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    // enable HMR globally
-
     new webpack.NamedModulesPlugin(),
-    // prints more readable module names in the browser console on HMR updates
-  ],
-};
+  ].concat(baseConfig.plugins),
+});
+
+const prodConfig = Object.assign({}, baseConfig, {
+});
+
+module.exports =
+  process.env.NODE_ENV === 'production'
+    ? prodConfig
+    : devConfig;
